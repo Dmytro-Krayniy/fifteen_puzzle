0903773ecf60bf0c956cda6620ccfdf90b817a60
MitLA <dmkrayniy@ukr.net>
2022-05-23 15:50:00 +0300

    added url_for function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, ValidationError
